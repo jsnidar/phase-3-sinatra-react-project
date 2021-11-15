a9b09b6b75a4b301fb79a0f2538a53295e1bfb24
Jeremiah <jsnidar@gmail.com>
2021-11-15 15:15:47 -0600

    Patch and post updated to reflect frontend changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
       image: params[:image],
       category_id: params[:category_id]
     )
-    params[:ingredients].each do |key, value|
+    params[:meal_ingredients].each do |key, value|
       MealIngredient.create(
         ingredient_id: key.to_i,
         meal_id: meal[:id],
@@ -46,8 +46,7 @@ class ApplicationController < Sinatra::Base
   patch '/meals/:id' do
     meal = Meal.find(params[:id])
     MealIngredient.where(meal_id: params[:id]).destroy_all
-
-    params[:ingredients].each do |meal_ing|
+    params[:meal_ingredients].each do |meal_ing|
         MealIngredient.create(
         ingredient_id: meal_ing[:ingredient_id],
         meal_id: meal[:id],
